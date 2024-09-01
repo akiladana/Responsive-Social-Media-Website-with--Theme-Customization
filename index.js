8fba44696788b2b2fe447b716f72a97f31f0d56d
Pathirage Akila Dananjana <101885553+akiladana@users.noreply.github.com>
2024-09-01 23:40:58 +0530

    sidebar menu items fuctionalites add using js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 //SIDEBAR
-
 const menuItems = document.querySelectorAll('.menu-item');
 
+//MESSAGES
+const messageNtification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = document.querySelectorAll('.messages');
+const messageSearch = document.querySelector('#message-search');
+
+//================SIDEBAR=======================
 //remove active class from all menu items
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -17,6 +23,16 @@ menuItems.forEach(item => {
             document.querySelector('.notification-popup').style.display = 'none';
         }else{
             document.querySelector('.notification-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
         }
     })
+})
+
+//================messages=======================
+messageNtification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messageNtification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
 })
